fcba594ed34992235535ebb6fe275999070f15e1
vernonthedev <techjaja2@gmail.com>
2024-05-28 12:05:27 -0700

    #Update: Comments are written like this

--- main.dart
+++ main.dart
@@ -1,6 +1,6 @@
 void main(){
     print("Hey bro can you help me out");
-
-    var x = 10;
-    print(x);
+    //this is how we write comments in dart
+    var age = 10;
+    print("Hey bro i am $age old");
 }
