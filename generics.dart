d5d13ab73f416941746d643f638a939ae2a22bd2
vernonthedev <techjaja2@gmail.com>
2024-05-28 18:35:43 -0700

    #Update: Proper way to iterate over collections in dart

--- generics.dart
+++ generics.dart
@@ -5,4 +5,12 @@ void main() {
   epicList.add(3);
   epicList.add(14);
   print(epicList);
+
+  //How to iterate over collections in dart
+  Iterator itr = epicList.iterator;
+  //loop thru the whole list as long as we still got values in it.
+  while (itr.moveNext()) {
+    //print the currently iterated val
+    print(itr.current);
+  }
 }
