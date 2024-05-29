28650bfeaab50dc2eb8cb0031ce3bf6f83c78860
vernonthedev <techjaja2@gmail.com>
2024-05-28 17:54:53 -0700

    #Update: introduced to maps

--- main.dart
+++ main.dart
@@ -1,6 +1,14 @@
-void main(){
-    print("Hey bro can you help me out");
-    //this is how we write comments in dart
-    var age = 10;
-    print("Hey bro i am $age old");
+void main() {
+  print("Hey bro can you help me out");
+  //this is how we write comments in dart
+  var age = 10;
+  print("Hey bro i am $age old");
+
+  //MAPS
+  var epicMap = {'key1': 123, 'key': 'brocode'};
+  print(epicMap);
+  print(epicMap['key']);
+
+  epicMap.forEach((key, value) => print(
+      "$key and $value")); //loop thru every single value and print the key&val
 }
